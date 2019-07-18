fce62dfd6fd3772570115a0dc6cf0b39da2f2fd2
bytesalad <52949496+bytesalad@users.noreply.github.com>
2019-07-18 15:46:20 +0200

    Update Experimentation
    So the gauge mechanic is working now. Its values can be easily accessed for other programs in the project. Each gauge has a Gauge_FIll script and a corresponding Interaction_Manager_GaugeInput script on the interaction manager. This was done for generic programming. and so that the feature can be easily changed in future.

---
+++ assessment1-wsoa2006/Assets/Scripts/Prototyping/Gauge/Gauge_Fill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gauge_Fill : MonoBehaviour
+{
+    [SerializeField] Image gaugeImage;
+
+    private float fillAmount;
+    //a number between 0 and 1 that tells the program how much the image should be filled
+
+    public void SetFillAmount(float fillAmount_)
+    {
+        fillAmount = fillAmount_;
+    }
+
+    public float GetFillAmount()
+    {
+        return fillAmount;
+    }
+
+    private void Update()
+        //runs once a frame hopefully this does not cause laggy issues.
+    {
+        gaugeImage.fillAmount = fillAmount;
+    }
+}
---
+++ assessment1-wsoa2006/Assets/Scripts/Prototyping/Interaction_Manager/Interaction_Manager_GaugeInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interaction_Manager_GaugeInput : MonoBehaviour
+{
+    [SerializeField] private Button leftButton;
+
+    [SerializeField] private Button rightButton;
+
+    [SerializeField] private Gauge_Fill gaugeFillScript;
+
+    [SerializeField] private float gaugeFillSpeed;
+
+    public void Start()
+    {
+        leftButton.onClick.AddListener(LeftButtonClick);
+        rightButton.onClick.AddListener(RightButtonClick);
+    }
+
+    private void LeftButtonClick()
+    {
+        float fillAmount = gaugeFillScript.GetFillAmount();
+
+        if (fillAmount > 0f)
+        {
+            gaugeFillScript.SetFillAmount(fillAmount - gaugeFillSpeed);
+        }
+    }
+
+    private void RightButtonClick()
+    {
+        float fillAmount = gaugeFillScript.GetFillAmount();
+
+        if (fillAmount < 1f)
+        {
+            gaugeFillScript.SetFillAmount(fillAmount + gaugeFillSpeed);
+        }
+    }
+}
