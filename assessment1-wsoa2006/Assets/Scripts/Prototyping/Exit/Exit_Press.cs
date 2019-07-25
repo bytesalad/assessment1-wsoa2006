cb5cc1cf9e4332ac790c5b740fa0ac11d905ac04
bytesalad <52949496+bytesalad@users.noreply.github.com>
2019-07-25 14:00:58 +0200

    Making prototype levels

---
+++ assessment1-wsoa2006/Assets/Scripts/Prototyping/Exit/Exit_Press.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit_Press : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
